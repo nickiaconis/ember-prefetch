58987a8a632e42846d6acadd9c8262a2f51d5868
Thomas Wang <wangxgwxg@gmail.com>
2019-11-18 11:06:32 -0800

    fix: prefetch hook on Lazy engine routes (#159)
    * internal: move regex off routWillChange
    
    * test: add failing test for lazy engine
    
    * fix: prefetch hook on lazy engine

--- addon/services/prefetch.js
+++ addon/services/prefetch.js
@@ -1,34 +1,35 @@
 import Service, { inject as service } from '@ember/service';
-import { schedule } from '@ember/runloop';
 import { createPrefetchChangeSet } from '../-private/diff-route-info';
-import RSVP from 'rsvp';
+import { Promise, all } from 'rsvp';
 import { gte } from 'ember-compatibility-helpers';
 
 let PrefetchService;
 
 if (gte('3.6.0')) {
+  let substatesRegex = /(^|_|\.)(loading$|error$)/;
   // remove guard for Ember 3.8 LTS and rev major
   PrefetchService = Service.extend({
     router: service('router'),
     init() {
       this._super(...arguments);
-      let privateRouter = this.router._router._routerMicrolib;
       let seenRoutes = new WeakMap();
 
       this.router.on('routeWillChange', transition => {
-        if (transition.to && /(^|_|\.)(loading$|error$)/.test(transition.to.name)) {
+        if (transition.to && substatesRegex.test(transition.to.name)) {
           return;
         }
 
-        schedule('actions', () => {
+        let routePromises = transition.routeInfos.map(info => info._routePromise);
+        all(routePromises).then(() => {
           if (!this.isDestroying && !this.isDestroyed) {
+            let privateRouter = this.router._router._routerMicrolib;
             let changeSet = createPrefetchChangeSet(privateRouter, transition);
             if (changeSet.shouldCall) {
               for (let i = 0; i < changeSet.for.length; i++) {
                 let { route, fullParams } = changeSet.for[i];
                 if (seenRoutes.has(route)) continue;
 
-                route._prefetched = new RSVP.Promise(r => {
+                route._prefetched = new Promise(r => {
                   return r(route.prefetch(fullParams, transition));
                 });
                 seenRoutes.set(route, true);
